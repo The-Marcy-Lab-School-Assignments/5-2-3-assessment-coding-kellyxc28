114f713a118bfd4a17c37e78204af9cb6ff82d82
Kelly Xiong Chen <kellyxiongc@gmail.com>
2024-07-18 12:51:08 -0400

    3rd Attempt: Inheritance & Polymorphism Test - addContact()

--- src/1-inheritance-polymorphism.js
+++ src/1-inheritance-polymorphism.js
@@ -1,25 +1,25 @@
 class Phone {
-  constructor(phoneNumber){
+  constructor(phoneNumber, name){
     this.phoneNumber = phoneNumber;
     this.contacts = [];
+    this.name = name;
   }
 
-  addContact() {
+  addContact(contact) {
     // name does not appear til we get to this method 
     // if (this.name === undefined || phoneNumber === undefined && phoneNumber.length !== 10) {
-    if (this.name === undefined || phoneNumber === undefined) {
+    if (this.name === undefined || this.phoneNumber === undefined) {
       return 'Invalid';
     }
     else { 
-      const newContact = new Phone(this.name, phoneNumber);
-      contacts.push(newContact);
-      // contacts.push(phoneNumber);
+      // const newContact = new Phone(this.name, this.phoneNumber);
+      // this.contacts.push(newContact);
+      this.contacts.push(contact);
       return `${this.name} added.`;
     }
   }
 
   removeContact() {
-
   }
 
   makeCall() {

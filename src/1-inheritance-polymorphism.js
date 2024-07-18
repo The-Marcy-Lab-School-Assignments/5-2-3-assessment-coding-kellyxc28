5f4b76e7fcd506f4d05a687d116d9e2275b7dec8
Kelly Xiong Chen <kellyxiongc@gmail.com>
2024-07-18 12:32:48 -0400

    2nd Attempt: Inheritance & Polymorphism Test - addContact()

--- src/1-inheritance-polymorphism.js
+++ src/1-inheritance-polymorphism.js
@@ -4,15 +4,16 @@ class Phone {
     this.contacts = [];
   }
 
-  addContact(phoneNumber) {
+  addContact() {
     // name does not appear til we get to this method 
     // if (this.name === undefined || phoneNumber === undefined && phoneNumber.length !== 10) {
     if (this.name === undefined || phoneNumber === undefined) {
       return 'Invalid';
     }
     else { 
-      // const newContact = new Phone(phoneNumber);
-      this.contacts.push(newContact);
+      const newContact = new Phone(this.name, phoneNumber);
+      contacts.push(newContact);
+      // contacts.push(phoneNumber);
       return `${this.name} added.`;
     }
   }

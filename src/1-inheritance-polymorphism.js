f967da93a076f447fdd165d714adefe8629e4221
Kelly Xiong Chen <kellyxiongc@gmail.com>
2024-07-18 12:10:06 -0400

    Attempted: Inheritance & Polymorphism Test - addContact()

--- src/1-inheritance-polymorphism.js
+++ src/1-inheritance-polymorphism.js
@@ -1,20 +1,28 @@
 class Phone {
-  contacts = [];
-
   constructor(phoneNumber){
     this.phoneNumber = phoneNumber;
+    this.contacts = [];
+  }
+
+  addContact(phoneNumber) {
+    // name does not appear til we get to this method 
+    // if (this.name === undefined || phoneNumber === undefined && phoneNumber.length !== 10) {
+    if (this.name === undefined || phoneNumber === undefined) {
+      return 'Invalid';
+    }
+    else { 
+      // const newContact = new Phone(phoneNumber);
+      this.contacts.push(newContact);
+      return `${this.name} added.`;
+    }
+  }
+
+  removeContact() {
+
   }
 
   makeCall() {
 
-  }
-
-  addContact() {
-
-  }
-
-  removeContact() {
-    
   }
 }
 

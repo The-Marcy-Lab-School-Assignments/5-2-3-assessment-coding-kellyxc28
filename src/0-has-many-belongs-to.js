5a5da0ad7cd444f9f2d3e9ed76b4525590c912dd
Kelly Xiong Chen <kellyxiongc@gmail.com>
2024-07-18 10:46:08 -0400

    Completed: Has Many / Belongs To Test - Start of Classes, ToDoLists's Create, Get, and Count

--- src/0-has-many-belongs-to.js
+++ src/0-has-many-belongs-to.js
@@ -1,15 +1,38 @@
 const { getId } = require('./utils');
 
 class ToDoItem {
-  constructor() {
+  constructor(description, priorityLevel) {
     this.id = getId();
+    this.description = description;
+    this.priorityLevel = priorityLevel;
+    this.isDone = false;
   }
 }
 
 
 class ToDoList {
+  #items = [];
+  static #allLists = [];
+
   constructor() {
     this.id = getId();
+    this.name = 'This week\'s tasks';
+
+    ToDoList.#allLists.push(this);
+  }
+
+  createItem(description, priorityLevel) {
+    const newItem = new ToDoItem(description, priorityLevel);
+    this.#items.push(newItem);
+    return newItem;
+  }
+
+  getItems(){
+    return [...this.#items];
+  }
+
+  getCompletedCount(){
+    return this.#items.reduce((accum, {isDone}) => accum + isDone, 0);
   }
 }
 
